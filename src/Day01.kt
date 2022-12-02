cbd58ecd85e69799e64d94f2e3789dd87dfc3aa3
Joakim Waltersson <joakim.waltersson@gmail.com>
2022-12-02 06:19:57 +0100

    Day 01 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -22,7 +22,6 @@ fun main() {
         return sums.take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
 

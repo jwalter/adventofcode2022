74f15dfcb99b5eefdaffaf5b0fb40702847d57a1
Joakim Waltersson <joakim.waltersson@gmail.com>
2022-12-04 06:45:42 +0100

    Clean up day 3 and 4 a bit

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,39 +1,43 @@
 fun main() {
     fun Pair<Int, Int>.rangeContains(other: Pair<Int, Int>): Boolean {
-        val result = this.first <= other.first && this.second >= other.second
-        return result
+        return first <= other.first && second >= other.second
     }
 
     fun fullyContains(a: Pair<Int, Int>, b: Pair<Int, Int>): Boolean {
         return a.rangeContains(b) || b.rangeContains(a)
     }
+
     fun overlaps(a: Pair<Int, Int>, b: Pair<Int, Int>): Boolean {
         val aRange = a.first..a.second
         val bRange = b.first..b.second
         return aRange.contains(b.first) ||
-               aRange.contains(b.second) ||
-               bRange.contains(a.first) ||
-               bRange.contains(a.second)
+                aRange.contains(b.second) ||
+                bRange.contains(a.first) ||
+                bRange.contains(a.second)
+    }
+
+    fun countRangesMatching(input: List<String>, predicate: (Pair<Int, Int>, Pair<Int, Int>) -> Boolean): Int {
+        return input
+            .map { it.split(",") }
+            .map {
+                it
+                    .map { assignment ->
+                        assignment
+                            .split("-")
+                            .map(String::toInt)
+                            .let { p -> p.first() to p.last() }
+                    }.let { elfPairAssignment ->
+                        predicate(elfPairAssignment.first(), elfPairAssignment.last())
+                    }
+            }.count { it }
     }
 
     fun part1(input: List<String>): Int {
-        val aa = input.map { it.split(",") }.map {
-            it.map { it.split("-").map { it.toInt() }.let { p -> p.first() to p.last() } }.let {
-                fullyContains(it.first(), it.last())
-            }
-        }.filter { it }
-
-        return aa.count()
+        return countRangesMatching(input, ::fullyContains)
     }
 
     fun part2(input: List<String>): Int {
-        val aa = input.map { it.split(",") }.map {
-            it.map { it.split("-").map { it.toInt() }.let { p -> p.first() to p.last() } }.let {
-                overlaps(it.first(), it.last())
-            }
-        }.filter { it }
-
-        return aa.count()
+        return countRangesMatching(input, ::overlaps)
     }
 
     val testInput = readInput("Day04_test")
